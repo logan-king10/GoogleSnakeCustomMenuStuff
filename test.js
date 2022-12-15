ecd48bb634239f4024831f0b3d62e9f3e2f0e609
logan <95551215+true-king-al@users.noreply.github.com>
2022-12-15 12:58:14 -0600

    Update test.js

--- test.js
+++ test.js
@@ -265,7 +265,7 @@ window.snake.more_menu = function() {
             case 4:  _le = .45;    break;
             case 5:  _le = 1.85;   break;
             case 6:  _le = _soep;  break;
-            case 7:  _le = 18.5;   break;
+            case 7:  _le = 15.5;   break;
             case 8:  _le = .35;    break;
             case 9:  _le = .25;    break;
             case 10: _le = .15;    break;
@@ -614,7 +614,7 @@ window.snake.more_menu = function() {
         c.replace(
           dec,
           `${dec.match(/f\.[a-zA-Z0-9_$]{1,8}/)} = {
-            width: (${beeeeeeaniebaby}) === 3 ? 10 : (${beeeeeeaniebaby}) === 4 ? 7 : (${beeeeeeaniebaby}) === 5 ? 12 : (${beeeeeeaniebaby}) === 6 ? 37 : (${beeeeeeaniebaby}) === 7 ? 64 : (${beeeeeeaniebaby}) === 8 ? 105 : (${beeeeeeaniebaby}) === 9 ? 168 : (${beeeeeeaniebaby}) === 10 ? 600 : Math.floor(b/${wa}),
+            width: (${beeeeeeaniebaby}) === 3 ? 5 : (${beeeeeeaniebaby}) === 4 ? 7 : (${beeeeeeaniebaby}) === 5 ? 12 : (${beeeeeeaniebaby}) === 6 ? 37 : (${beeeeeeaniebaby}) === 7 ? 64 : (${beeeeeeaniebaby}) === 8 ? 105 : (${beeeeeeaniebaby}) === 9 ? 168 : (${beeeeeeaniebaby}) === 10 ? 600 : Math.floor(b/${wa}),
             height: (${beeeeeeaniebaby}) === 3 ? 4 : (${beeeeeeaniebaby}) === 4 ? 6 : (${beeeeeeaniebaby}) === 5 ? 11 : (${beeeeeeaniebaby}) === 6 ? 32 : (${beeeeeeaniebaby}) === 7 ? 56 : (${beeeeeeaniebaby}) === 8 ? 92  : (${beeeeeeaniebaby}) === 9 ? 147 : (${beeeeeeaniebaby}) === 10 ? 530 : Math.floor(c/${wa})
           });
           let squareSize = 600 / (${dec.match(/f\.[a-zA-Z0-9_$]{1,8}/)}).width;
